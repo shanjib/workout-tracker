5f6d2ae2126c4d0cc8deb311c6054aafac2a140a
shanjib <ishanjib@gmail.com>
2026-04-19 18:48:17 -0400

    update infrastructure stack to create user pool, add amplifyConfig to connect to Cognito

---
+++ frontend/src/amplifyconfiguration.ts
@@ -0,0 +1,10 @@
+const amplifyConfig = {
+    Auth: {
+        Cognito: {
+            userPoolId: import.meta.env.VITE_USER_POOL_ID,
+            userPoolClientId: import.meta.env.VITE_USER_POOL_CLIENT_ID,
+        }
+    }
+}
+
+export default amplifyConfig
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
+import { Amplify } from 'aws-amplify'
+import amplifyConfig from './amplifyconfiguration'
 import './style.css'
 import App from './App.vue'
+
+Amplify.configure(amplifyConfig)
 
 createApp(App).mount('#app')
